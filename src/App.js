6dbd25aa3a0e703e4b880bedae09a3bbdb7b97cd
pankajdrsm <pankajmeena161491@gmail.com>
2023-10-09 03:06:04 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import UserData from './UserData';
+import ProblemRecommendations from './ProblemRecommendations';
+import PieChart from './PieChart';
 
 function App() {
+  const [userHandle, setUserHandle] = useState('');
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const [userData, setUserData] = useState(null);
+  const [userSubmissions, setUserSubmissions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [barChartData, setBarChartData] = useState(null);
+
+  const fetchUserData = async () => {
+    try {
+      // Fetch user data from Codeforces using the user handle
+      const response = await axios.get(`https://codeforces.com/api/user.info?handles=${userHandle}`);
+      const userData = response.data.result[0];
+      setUserData(userData);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  const Clicked = () => {
+    setButtonClicked(false);
+    setButtonClicked(true);
+  };
+
+  // Rest of your code for calculating barChartData and displaying it goes here
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Codeforces Problem Recommender</h1>
+      <div className="user-input">
+        <input
+          type="text"
+          placeholder="Enter your Codeforces user handle"
+          value={userHandle}
+          onChange={(e) => {setUserHandle(e.target.value);
+            setButtonClicked(false);}}
+        />
+        <button onClick={Clicked}>Search</button>
+      </div>
+      {buttonClicked && <UserData userHandle={userHandle} />}
+      
+      <div className="side-by-side-container">
+        {/* First component */}
+        <div className="side-by-side-item">
+       
+        {buttonClicked && <PieChart userHandle={userHandle} />}
+        </div>
+
+        {/* Second component */}
+        <div className="side-by-side-item">
+         {/* {buttonClicked && <ProblemRecommendations userHandle={userHandle} />} */}
+        </div>
+
+        {/* Third component */}
+        <div className="side-by-side-item">
+        {/* {buttonClicked && <ProblemRecommendations userHandle={userHandle} />} */}
+        </div>
+      </div>
     </div>
   );
 }
